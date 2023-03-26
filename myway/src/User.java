d3cfb31c59ade54344a5ef77177d64e893e6c966
Ozan <63856146+Ozancann01@users.noreply.github.com>
2023-03-26 12:07:40 +0200

    aantal functionaliteiten toegevoegt
    stappenteller, trainingsagenda en user

---
+++ myway/src/User.java
@@ -0,0 +1,21 @@
+package myway.src;
+
+public abstract class User {
+    private String naam;
+    private int leeftijd;
+    private double gewicht;
+    private double lengte;
+
+    public User(String naam, int leeftijd, double gewicht, double lengte) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.gewicht = gewicht;
+        this.lengte = lengte;
+    }
+
+    public double berekenBMI() {
+        return gewicht / Math.pow(lengte / 100, 2);
+    }
+
+    public abstract String getStatus();
+}
